100f842b86579e0bb6bc1b6a902d1a1439ffe7e8
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2024-12-24 14:00:38 -0500

    draft image phom step + reorganize details to forestall tag conflicts

---
+++ man-roxygen/step-phom-details.R
@@ -0,0 +1,6 @@
+#' @details
+#' Persistent homology (PH) is a tool of algebraic topology to extract features
+#' from data whose _persistence_ measures their robustness to scale. The
+#' computation relies on a sequence of maps between discrete topological spaces
+#' (usually a filtration comprising only inclusions) constructed from the data.
+#' 
