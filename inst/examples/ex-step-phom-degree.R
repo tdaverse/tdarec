9e3630f8728f094a2adfab10437f81d4f50c083a
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2025-01-03 10:05:35 -0500

    revise examples & write tests for degree phom step

--- inst/examples/ex-step-phom-degree.R
+++ inst/examples/ex-step-phom-degree.R
@@ -1,21 +1,21 @@
-roads <- data.frame(
-  dist = I(list(eurodist, UScitiesD)),
-  pix = I(list(volcano, 255 - volcano))
+dat <- data.frame(
+  roads = I(list(eurodist, UScitiesD)),
+  topos = I(list(volcano, 255 - volcano))
 )
 
-ph_rec <- recipe(~ ., data = roads) |> 
-  step_phom_point_cloud(dist, max_hom_degree = 2, keep_original_cols = FALSE) |> 
-  step_phom_image(pix, keep_original_cols = FALSE) |> 
-  step_phom_degree(dist_phom, pix_phom)
-ph_prep <- prep(ph_rec, training = roads)
-ph_res <- bake(ph_prep, roads)
+ph_rec <- recipe(~ ., data = dat) |> 
+  step_phom_point_cloud(roads, keep_original_cols = FALSE) |> 
+  step_phom_image(topos, keep_original_cols = FALSE) |> 
+  step_phom_degree(roads_phom, topos_phom)
+ph_prep <- prep(ph_rec, training = dat)
+(ph_res <- bake(ph_prep, dat))
 
 tidy(ph_rec, number = 3)
 tidy(ph_prep, number = 3)
 
-with_degs <- recipe(~ ., data = roads) |> 
-  step_phom_point_cloud(dist, max_hom_degree = 2, keep_original_cols = FALSE) |> 
-  step_phom_image(pix, keep_original_cols = FALSE) |> 
-  step_phom_degree(dist_phom, pix_phom, hom_degrees = c(1, 2))
-with_degs <- prep(with_degs, training = roads)
-bake(with_degs, roads)
+with_degs <- recipe(~ ., data = dat) |> 
+  step_phom_point_cloud(roads, keep_original_cols = FALSE) |> 
+  step_phom_image(topos, keep_original_cols = FALSE) |> 
+  step_phom_degree(roads_phom, topos_phom, hom_degrees = c(1, 2))
+with_degs <- prep(with_degs, training = dat)
+bake(with_degs, dat)
