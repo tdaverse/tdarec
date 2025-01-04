6a2f78688438453f0cd53bdd8e9b5764dc09de66
Jason Cory Brunson <cornelioid@gmail.com>
2025-01-04 08:48:33 -0500

    rename image to lattice

--- inst/examples/ex-step-phom-degree.R
+++ inst/examples/ex-step-phom-degree.R
@@ -5,7 +5,7 @@ dat <- data.frame(
 
 ph_rec <- recipe(~ ., data = dat) |> 
   step_phom_point_cloud(roads, keep_original_cols = FALSE) |> 
-  step_phom_image(topos, keep_original_cols = FALSE) |> 
+  step_phom_lattice(topos, keep_original_cols = FALSE) |> 
   step_phom_degree(roads_phom, topos_phom)
 ph_prep <- prep(ph_rec, training = dat)
 (ph_res <- bake(ph_prep, dat))
@@ -15,7 +15,7 @@ tidy(ph_prep, number = 3)
 
 with_degs <- recipe(~ ., data = dat) |> 
   step_phom_point_cloud(roads, keep_original_cols = FALSE) |> 
-  step_phom_image(topos, keep_original_cols = FALSE) |> 
+  step_phom_lattice(topos, keep_original_cols = FALSE) |> 
   step_phom_degree(roads_phom, topos_phom, hom_degrees = c(1, 2))
 with_degs <- prep(with_degs, training = dat)
 bake(with_degs, dat)
