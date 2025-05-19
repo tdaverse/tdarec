5738e5b5a7bdf35ca27ac4da35f3db5abde36ad8
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2025-05-19 09:58:36 -0400

    hopefully resolve pre-build compilation problem of images not being found

--- pre-compile/compile-vignettes.R
+++ pre-compile/compile-vignettes.R
@@ -2,7 +2,21 @@
 # in order to limit CRAN checktime. It follows rOpenSci guidance:
 # https://ropensci.org/blog/2019/12/08/precompute-vignettes/
 
+# knit the static vignette
 knitr::knit(
-  "vignettes/hyperparameter-tuning-orig.Rmd",
+  input  = "vignettes/hyperparameter-tuning-orig.Rmd",
   output = "vignettes/hyperparameter-tuning.Rmd"
 )
+
+# obtain image file names
+vignette_images <- list.files(
+  path = "./",
+  pattern = "vignette\\-.*\\.png"
+)
+
+# move the images to the `vignettes` folder
+file.copy(
+  from = vignette_images,
+  to   = "vignettes/"
+)
+file.remove(vignette_images)
