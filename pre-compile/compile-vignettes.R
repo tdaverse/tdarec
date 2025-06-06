60617b87e220729acb48351fefeb3946af0f1e18
Jason Cory Brunson <cornelioid@gmail.com>
2025-06-06 06:42:21 -0400

    rename costly vignette source to preclude pkgdown article - fixes #13

--- pre-compile/compile-vignettes.R
+++ pre-compile/compile-vignettes.R
@@ -1,10 +1,12 @@
 # This script compiles the time-costly vignette prior to build,
 # in order to limit CRAN checktime. It follows rOpenSci guidance:
 # https://ropensci.org/blog/2019/12/08/precompute-vignettes/
+# Note that the initial underscore that flags the source file
+# also signals {pkgdown} to ignore it when building articles.
 
 # knit the static vignette
 knitr::knit(
-  input  = "vignettes/hyperparameter-tuning-orig.Rmd",
+  input  = "vignettes/_hyperparameter-tuning.Rmd",
   output = "vignettes/hyperparameter-tuning.Rmd"
 )
 
